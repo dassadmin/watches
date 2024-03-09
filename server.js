6ef6bcd3e3a047fb89017d967c644a79c37953c5
dassadmin <161880455+dassadmin@users.noreply.github.com>
2024-03-09 19:14:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
-import { fileURLToPath} from 'url';
+
 //configure env
 dotenv.config();
 
@@ -16,8 +16,6 @@ dotenv.config();
 connectDB();
 
 
-const __fileUrlToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
