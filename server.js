821628ddb26049915f6dbc4472123b1c745c6b03
dassadmin <161880455+dassadmin@users.noreply.github.com>
2024-03-09 19:57:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 //configure env
 dotenv.config();
@@ -15,6 +16,8 @@ dotenv.config();
 //databse config
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
@@ -22,6 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join( __dirname, "./client/build")));
 
 
 //routes
